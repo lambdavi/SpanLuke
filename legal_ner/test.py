5f646829d3dbea55187b0b05c41293e87552f9d2
Davide Buoso <daviibuoso@gmail.com>
2023-12-03 16:41:42 +0100

    test uner

--- legal_ner/test.py
+++ legal_ner/test.py
@@ -1,7 +1,7 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 
-pipe = pipeline("text-generation", model="Universal-NER/UniNER-7B-all")
+pipe = pipeline("text-generation", model="Universal-NER/UniNER-7B-type")
 test = "Text: What is a legal framework by Judge Solos Poppier"
 entity = "JUDGE"
 prompt = f"What describes {entity} in the text"
