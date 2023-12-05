a03a41165dfdb05870b2f57ba4996d96cc0fe443
Davide Buoso <daviibuoso@gmail.com>
2023-12-05 16:02:35 +0100

    inference

--- legal_ner/main.py
+++ legal_ner/main.py
@@ -216,6 +216,7 @@ if __name__ == "__main__":
 
         ## Training Arguments
         training_args = TrainingArguments(
+            lr_scheduler_type="linear",
             output_dir=new_output_folder,
             num_train_epochs=num_epochs,
             learning_rate=lr,
