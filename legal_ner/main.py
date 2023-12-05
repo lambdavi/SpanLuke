8cc7b1c8c8de27d4f3dc0021bdf5a72e85ed624c
Davide Buoso <daviibuoso@gmail.com>
2023-12-05 22:48:14 +0100

    span

--- legal_ner/main.py
+++ legal_ner/main.py
@@ -154,9 +154,9 @@ if __name__ == "__main__":
 
     ## Define the models
     model_paths = [
+        'guishe/span-marker-generic-ner-v1-fewnerd-fine-super',
         'saibo/legal-roberta-base',
         'joelniklaus/legal-xlm-longformer-base',
-        'guishe/span-marker-generic-ner-v1-fewnerd-fine-super',
         'phjhk/hklegal-xlm-r-base-t',
         'nickprock/distilbert-finetuned-ner-ontonotes',
         'phjhk/hklegal-xlm-r-base'    
