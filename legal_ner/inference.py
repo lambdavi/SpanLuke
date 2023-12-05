ad5b89c03de2bb56ce7f9905e0a5eb18bd1668ad
Davide Buoso <daviibuoso@gmail.com>
2023-12-05 15:48:54 +0100

    inference

--- legal_ner/inference.py
+++ legal_ner/inference.py
@@ -108,8 +108,7 @@ base_dir = "results/all"
     'studio-ousia/luke-large'),                 # LUKE large
 ]"""
 all_model_path = [        # ft on Legal Domain
-    (f'{base_dir}/saibo/legal-roberta-base/checkpoint-27',
-    'saibo/legal-roberta-base'),                # ft on Legal Domain                 # LUKE large
+    (f'{base_dir}/saibo/legal-roberta-base/checkpoint-27', 'saibo/legal-roberta-base'),                # ft on Legal Domain                 # LUKE large
 ]
 
 ## Loop over the models
@@ -145,6 +144,7 @@ for model_path in sorted(all_model_path):
     ]
 
     ## Initialize the NER extractor
+    print(model_path)
     ner_extr = NERExtractor(
         ner_model_path = model_path[0], 
         tokenizer = tokenizer, 
