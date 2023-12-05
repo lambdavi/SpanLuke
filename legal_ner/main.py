145b48a7e3f7926b85ac6ac68e3891e975990952
Davide Buoso <daviibuoso@gmail.com>
2023-12-05 10:17:25 +0100

    span

--- legal_ner/main.py
+++ legal_ner/main.py
@@ -165,7 +165,7 @@ if __name__ == "__main__":
 
         ## Define the train and test datasets
         use_roberta = False
-        if "luke" in model_path or "roberta" in model_path or "xlm" in model_path:
+        if "luke" in model_path or "roberta" in model_path or "xlm" in model_path or "span" in model_path:
             use_roberta = True
 
         train_ds = LegalNERTokenDataset(
