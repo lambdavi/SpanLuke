1fd5146f56b1f45ffaf0b1b4c3ab6bb9c2ec824a
Davide Buoso <daviibuoso@gmail.com>
2023-12-09 13:48:00 +0100

    gradient accumulation = 2

--- legal_ner/main.py
+++ legal_ner/main.py
@@ -236,7 +236,7 @@ if __name__ == "__main__":
             learning_rate=lr,
             per_device_train_batch_size=batch_size,
             per_device_eval_batch_size=batch_size,
-            gradient_accumulation_steps=256//batch_size,
+            gradient_accumulation_steps=2,
             gradient_checkpointing=True if "span" not in model_path else False,
             warmup_ratio=warmup_ratio,
             weight_decay=weight_decay,
@@ -251,7 +251,6 @@ if __name__ == "__main__":
             dataloader_pin_memory=True,
             report_to="wandb",
             logging_steps=50,  # how often to log to W&B
-
         )
 
         ## Collator
