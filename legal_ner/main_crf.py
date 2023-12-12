c4b203a91f0a89c89cc88670ca5bf34b0c7fdd0e
Davide Buoso <daviibuoso@gmail.com>
2023-12-12 17:18:14 +0100

    crf test

--- legal_ner/main_crf.py
+++ legal_ner/main_crf.py
@@ -16,9 +16,9 @@ nlp = spacy.load("en_core_web_sm")
 ## Define the model with CRF layer
 class CustomModelWithCRF(AutoModelForTokenClassification):
     def __init__(self, config):
-        super().__init__(config)
+        super().__init__()
         self.crf = CRF(config.num_labels, batch_first=True)
-        self.model = super().from_pretrained(config.pretrained_model_name_or_path, num_labels=num_labels)
+        self.model = super().from_pretrained(config.pretrained_model_name_or_path, num_labels=num_labels, config=config)
     def forward(self, tensor):
         return self.model.forward_features(tensor)
 
