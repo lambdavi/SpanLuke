dd601c93b7291ddf1439b53bdf8881889b79d85b
Davide Buoso <daviibuoso@gmail.com>
2023-12-15 14:36:08 +0100

    lstm

--- legal_ner/main_lstm.py
+++ legal_ner/main_lstm.py
@@ -17,7 +17,7 @@ nlp = spacy.load("en_core_web_sm")
 class CustomModelWithBiLSTM(nn.Module):
     def __init__(self, model_path, num_labels, hidden_size=768, lstm_hidden_size=256, num_lstm_layers=1, bidirectional=True, dropout=0.1):
         super(CustomModelWithBiLSTM, self).__init__()
-        self.device = "cpu" if cuda.is_available() else "cuda"
+        self.device = "cpu" if not cuda.is_available() else "cuda"
         self.bert = AutoModelForTokenClassification.from_pretrained(model_path, output_hidden_states=True)
         self.dropout = nn.Dropout(dropout)
         self.bilstm = nn.LSTM(
