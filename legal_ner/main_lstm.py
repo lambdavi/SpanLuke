64d9091beb0a75940bbd0ff9ce4dddac04fea2a8
Davide Buoso <daviibuoso@gmail.com>
2023-12-15 22:42:29 +0100

    freeze final

--- legal_ner/main_lstm.py
+++ legal_ner/main_lstm.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser
 from nervaluate import Evaluator
 from torchcrf import CRF  # Import CRF layer
 from transformers import EarlyStoppingCallback
-from transformers import AutoModelForTokenClassification
+from transformers import AutoModelForTokenClassification, AutoModel
 from transformers import Trainer, DefaultDataCollator, TrainingArguments
 from torch import nn
 from utils.dataset import LegalNERTokenDataset
@@ -18,7 +18,7 @@ class CustomModelWithBiLSTM(nn.Module):
     def __init__(self, model_path, num_labels, freeze=False, hidden_size=768, lstm_hidden_size=256, num_lstm_layers=1, bidirectional=True, dropout=0.1):
         super(CustomModelWithBiLSTM, self).__init__()
         self.device = "cpu" if not cuda.is_available() else "cuda"
-        self.bert = AutoModelForTokenClassification.from_pretrained(model_path, output_hidden_states=True)
+        self.bert = AutoModel.from_pretrained(model_path, output_hidden_states=True)
         print(self.bert)
         if freeze:
             self.bert.roberta.requires_grad_(False)
