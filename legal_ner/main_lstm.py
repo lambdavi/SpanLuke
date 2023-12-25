938150cc5a97ded1a002305520a8ce9ab59b2363
Davide Buoso <daviibuoso@gmail.com>
2023-12-25 23:41:44 +0100

    freezing improved

--- legal_ner/main_lstm.py
+++ legal_ner/main_lstm.py
@@ -283,7 +283,7 @@ if __name__ == "__main__":
             lr_scheduler_type=scheduler_type,
             per_device_train_batch_size=batch_size,
             per_device_eval_batch_size=batch_size,
-            gradient_accumulation_steps=1,
+            gradient_accumulation_steps=4,
             #gradient_checkpointing=True if "span" not in model_path else False,
             warmup_ratio=warmup_ratio,
             weight_decay=weight_decay,
