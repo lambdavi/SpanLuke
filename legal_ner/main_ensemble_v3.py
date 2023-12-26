6c7d3e412127b59dcaa2fb5677ef65817b90e40c
Davide Buoso <daviibuoso@gmail.com>
2023-12-26 10:55:06 +0100

    lstm with add args

--- legal_ner/main_ensemble_v3.py
+++ legal_ner/main_ensemble_v3.py
@@ -6,51 +6,73 @@ from nervaluate import Evaluator
 from torchcrf import CRF  # Import CRF layer
 from transformers import EarlyStoppingCallback
 from transformers import AutoModelForTokenClassification, AutoModel
-from transformers import Trainer, DefaultDataCollator, TrainingArguments, DataCollatorForTokenClassification
-from torch import nn
+from transformers import Trainer, DefaultDataCollator, TrainingArguments
+from torch import nn,cuda, zeros_like, bool, where, tensor, BoolTensor
 from utils.dataset import LegalNERTokenDataset
-from torch import cuda
+
 import spacy
 nlp = spacy.load("en_core_web_sm")
 
-# Define the model with BiLSTM layer
-class CustomModelWithBiLSTM(nn.Module):
-    def __init__(self, model_path, num_labels, freeze=False, hidden_size=1024, lstm_hidden_size=256, num_lstm_layers=1, bidirectional=True, dropout=0.1):
-        super(CustomModelWithBiLSTM, self).__init__()
+class Primary(nn.Module):
+    def __init__(self, model_path, num_labels, freeze=False, hidden_size=768, dropout=0.1, spec_mask=None):
+        super(Primary, self).__init__()
         self.device = "cpu" if not cuda.is_available() else "cuda"
-        self.bert = AutoModel.from_pretrained(model_path, output_hidden_states=True)
-        self.bert.gradient_checkpointing = True
+        self.bert = AutoModel.from_pretrained(model_path, ignore_mismatched_sizes=True)
         if freeze:
-            print("Freezeing layers.. ")
-            for name, param in self.bert.named_parameters():
-                if 'classifier' not in name:
-                    param.requires_grad = False
+            self.bert.encoder.requires_grad_(False)
         # https://github.com/huggingface/transformers/issues/1431
         self.dropout = nn.Dropout(dropout)
-        self.bilstm = nn.LSTM(
-            input_size=hidden_size,
-            hidden_size=lstm_hidden_size,
-            num_layers=num_lstm_layers,
-            bidirectional=bidirectional,
-            batch_first=True,
-            dropout=dropout if num_lstm_layers > 1 else 0,
-        )
-        self.linear = nn.Linear(lstm_hidden_size * 2 if bidirectional else lstm_hidden_size, num_labels)
+        self.linear = nn.Linear(hidden_size, num_labels)
         self.crf = CRF(num_labels, batch_first=True)
+        self.weight_factor = 0.5
+        self.specialized_labels = spec_mask
 
     def forward(self, input_ids, attention_mask, token_type_ids=None, labels=None):
         outputs = self.bert(input_ids=input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
-        last_hidden_states = outputs.hidden_states[-1]
-        lstm_out, _ = self.bilstm(last_hidden_states)
-        logits = self.linear(self.dropout(lstm_out))
+        sequence_out = outputs[0]
+        logits = self.linear(self.dropout(sequence_out))
+
+        sec_model.eval()
+        logits2 = sec_model(input_ids=input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask, labels=labels)
+        # Apply softmax to obtain probabilities
+        combined_logits = logits.clone()
+        specialized_mask = zeros_like(combined_logits, dtype=bool)
+        for label in self.specialized_labels:
+            specialized_mask[:, :, labels_to_idx[label]] = True
+    
+        combined_logits[specialized_mask] = (1 - self.weight_factor) * logits[specialized_mask] + self.weight_factor * logits2[1][specialized_mask]
+        
         if labels != None:
-            crf_loss = -self.crf(logits, labels, mask=attention_mask.bool(), reduction="mean" if batch_size!=1 else "token_mean") # if not mean, it is sum by default
+            crf_loss = -self.crf(combined_logits, labels, mask=attention_mask.bool(), reduction="mean" if batch_size!=1 else "token_mean") # if not mean, it is sum by default
             return (crf_loss, logits)
         else:
-            outputs = self.crf.decode(logits, attention_mask.bool())
+            outputs = self.crf.decode(combined_logits, attention_mask.bool())
             return outputs
-        
 
+class SecondaryTrainer(Trainer):
+    def __init__(self, specialized_mask, *args, **kwargs):
+        super(SecondaryTrainer, self).__init__(*args, **kwargs)
+        self.specialized_labels=specialized_mask
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.pop("labels")
+        # forward pass
+        outputs = model(**inputs)
+        logits = outputs.get("logits")
+        
+        # Get indices of specialized labels
+        selected_indices = [labels_to_idx[label] for label in self.specialized_labels]
+
+        # Index logits and labels to get the selected logits and labels
+        selected_logits = logits[:, :, selected_indices]
+        selected_labels_batch = labels[:, selected_indices]
+
+        # Compute custom loss only for selected labels
+        custom_loss = nn.functional.cross_entropy(selected_logits, selected_labels_batch, reduction='mean')
+
+        return (custom_loss, outputs) if return_outputs else custom_loss
+      
+    
 ############################################################
 #                                                          #
 #                           MAIN                           #
@@ -80,10 +102,18 @@ if __name__ == "__main__":
         required=False,
         type=str,
     )
+
     parser.add_argument(
         "--model_path",
         help="The model path from huggingface/local folder",
         default=None,
+        required=False,
+        type=str,
+    )
+    parser.add_argument(
+        "--model_path_secondary",
+        help="The model path from huggingface/local folder (secondary model)",
+        default="roberta-base",
         required=False,
         type=str,
     )
@@ -137,24 +167,11 @@ if __name__ == "__main__":
         required=False,
         type=float,
     )
-    parser.add_argument(
-        "--lstm_hidden",
-        help="lstm hidden size",
-        default=768,
-        required=False,
-        type=int,
-    )
-    parser.add_argument(
-        "--freeze",
-        help="Freeze the encoder layers",
-        action="store_true",
-        default=False
-    )
 
     parser.add_argument(
-        "--acc_step",
-        help="Gradient accumulation steps",
-        default=1,
+        "--hidden",
+        help="Warmup ratio",
+        default=768,
         required=False,
         type=int,
     )
@@ -172,7 +189,9 @@ if __name__ == "__main__":
     warmup_ratio = args.warmup_ratio    # e.g., 0.06
     workers = args.workers              # e.g., 4
     scheduler_type = args.scheduler     # e.g., linear
-    acc_step = args.acc_step
+    single_model_path = args.model_path        # e.g. bert-base-uncased
+    model_path_secondary = args.model_path_secondary        # e.g. bert-base-uncased
+
     ## Define the labels
     original_label_list = [
         "COURT",
@@ -196,12 +215,10 @@ if __name__ == "__main__":
 
     ## Compute metrics
     def compute_metrics(pred):
-
         # Preds
         predictions = np.argmax(pred.predictions, axis=-1)
         predictions = np.concatenate(predictions, axis=0)
         prediction_ids = [[idx_to_labels[p] if p != -100 else "O" for p in predictions]]
-
         # Labels
         labels = pred.label_ids
         labels = np.concatenate(labels, axis=0)
@@ -237,9 +254,10 @@ if __name__ == "__main__":
         }
 
     if args.model_path:
-        model_paths=[args.model_path]
+        model_paths=[single_model_path]
     else:
         model_paths = [
+            "nlpaueb/bert-base-uncased-echr", # to delete
             "studio-ousia/luke-large",
             'law-ai/InLegalBERT',
             'microsoft/deberta-v3-base',
@@ -247,8 +265,8 @@ if __name__ == "__main__":
             "geckos/deberta-base-fine-tuned-ner", # not bad, to finetune better
             "studio-ousia/luke-base",
         ]
-    for model_path in model_paths:
 
+    for model_path in model_paths:
         print("MODEL: ", model_path)
 
         ## Define the train and test datasets
@@ -272,33 +290,56 @@ if __name__ == "__main__":
             use_roberta=use_roberta
         )
 
-        model = CustomModelWithBiLSTM(model_path, num_labels=num_labels, hidden_size=args.lstm_hidden, freeze=args.freeze)
-        print("Final Model: ", model, sep="\n")
+        train_ds_small = LegalNERTokenDataset(
+            "data/NER_TRAIN/NER_TRAIN_SMALL2.json", 
+            model_path_secondary, 
+            labels_list=labels_list, 
+            split="train", 
+            use_roberta=use_roberta
+        )
 
+        val_ds_small = LegalNERTokenDataset(
+            "data/NER_DEV/NER_DEV_SMALL2.json", 
+            model_path_secondary, 
+            labels_list=labels_list, 
+            split="val", 
+            use_roberta=use_roberta
+        )
+        print("LABELS: ", labels_list)
+        # Create label mask
+        labels_to_specialize = ["ORG", "GPE", "PRECEDENT"]
+        labels_mask = ["B-" + l for l in labels_to_specialize]
+        labels_mask += ["I-" + l for l in labels_to_specialize]
+        print("LABEL MASK", labels_mask)
+        sec_model = AutoModelForTokenClassification.from_pretrained(model_path, ignore_mismatched_sizes=True, num_labels=num_labels)       
+        print("SECONDARY MODEL", sec_model, sep="\n")
+
+    
+        main_model = Primary(model_path, num_labels=num_labels, hidden_size=args.hidden, spec_mask=labels_mask)
+        print("MAIN MODEL", main_model, sep="\n")
+        
         ## Map the labels
         idx_to_labels = {v[1]: v[0] for v in train_ds.labels_to_idx.items()}
-
+        labels_to_idx = train_ds.labels_to_idx
         ## Output folder
         new_output_folder = os.path.join(output_folder, 'all')
         new_output_folder = os.path.join(new_output_folder, model_path)
         if not os.path.exists(new_output_folder):
             os.makedirs(new_output_folder)
-
         ## Training Arguments
-        training_args = TrainingArguments(
+        training_args= TrainingArguments(
             output_dir=new_output_folder,
             num_train_epochs=num_epochs,
             learning_rate=lr,
             lr_scheduler_type=scheduler_type,
             per_device_train_batch_size=batch_size,
             per_device_eval_batch_size=batch_size,
-            gradient_accumulation_steps=acc_step,
-            gradient_checkpointing=False,
+            gradient_accumulation_steps=1,
             warmup_ratio=warmup_ratio,
             weight_decay=weight_decay,
             evaluation_strategy="epoch",
             save_strategy="epoch",
-            load_best_model_at_end=True,
+            load_best_model_at_end=False,
             save_total_limit=2,
             fp16=False,
             fp16_full_eval=False,
@@ -306,40 +347,41 @@ if __name__ == "__main__":
             dataloader_num_workers=workers,
             dataloader_pin_memory=True,
             report_to="wandb",
-            logging_steps=50,  # how often to log to W&B
+            logging_steps=3000 if batch_size==1 else 50,
         )
-
         ## Collator
         data_collator = DefaultDataCollator()
 
         ## Trainer
+        trainer_sec = SecondaryTrainer(
+            model=sec_model,
+            args=training_args,
+            train_dataset=train_ds_small,
+            eval_dataset=val_ds_small,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics,
+            specialized_mask=labels_mask
+        )
+        ## Train the model and save it
+        print("**\tCRF ON\t**")
+        print("STARTING THE AUXILIARY MODEL TRAINING")
+        trainer_sec.train()
 
-        trainer = Trainer(
-            model=model,
+        training_args.load_best_model_at_end=True
+        training_args.num_train_epochs=num_epochs//2
+        trainer_main = Trainer(
+            model=main_model,
             args=training_args,
             train_dataset=train_ds,
             eval_dataset=val_ds,
             data_collator=data_collator,
             compute_metrics=compute_metrics,
+            callbacks=[EarlyStoppingCallback(2)]
         )
-
-        ## Train the model and save it
-        print("** BILSTM-CRF ON - STARTING **")
-        trainer.train()
-        trainer.save_model(output_folder)
-        trainer.evaluate()
+        print("STARTING THE MAIN MODEL TRAINING")
+        trainer_main.train()
+        trainer_main.save_model(output_folder)
+        trainer_main.evaluate()
 
 
-
-"""python 3.10
-Example of usage:
-python main_crf.py \
-    --ds_train_path data/NER_TRAIN/NER_TRAIN_ALL.json \
-    --ds_valid_path data/NER_DEV/NER_DEV_ALL.json \
-    --output_folder results/ \
-    --batch 256 \
-    --num_epochs 5 \
-    --lr 1e-4 \
-    --weight_decay 0.01 \
-    --warmup_ratio 0.06
-"""
+        
