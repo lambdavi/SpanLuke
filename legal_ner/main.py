d3b71346971c40d1a4b34e13603ea7636ed3c099
Davide Buoso <daviibuoso@gmail.com>
2023-12-29 19:19:19 +0100

    small fix in main

--- legal_ner/main.py
+++ legal_ner/main.py
@@ -123,8 +123,11 @@ if __name__ == "__main__":
     workers = args.workers              # e.g., 4
     scheduler_type = args.scheduler     # e.g., linear
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     acc_step = args.acc_step
 =======
+=======
+>>>>>>> Stashed changes
 
 >>>>>>> Stashed changes
     ## Define the labels
@@ -278,6 +281,9 @@ if __name__ == "__main__":
             gradient_accumulation_steps=acc_step,
 =======
             gradient_accumulation_steps=1,
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
             gradient_checkpointing=True if "span" not in model_path else False,
             warmup_ratio=warmup_ratio,
