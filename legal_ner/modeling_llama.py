99fedd9099088749a09634ac842b0f6f38fbe6e0
Davide Buoso <daviibuoso@gmail.com>
2024-01-03 11:52:55 +0100

    testing new metrics computation

--- legal_ner/modeling_llama.py
+++ legal_ner/modeling_llama.py
@@ -56,7 +56,7 @@ class UnmaskingLlamaModel(LlamaPreTrainedModel):
         self.vocab_size = config.vocab_size
 
         self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
-        self.layers = nn.ModuleList([LlamaDecoderLayer(config) for _ in range(config.num_hidden_layers)])
+        self.layers = nn.ModuleList([LlamaDecoderLayer(config, layer_idx=None) for _ in range(config.num_hidden_layers)])
         self.norm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
 
         self.gradient_checkpointing = False
