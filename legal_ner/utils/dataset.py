bf26def97d25d6ea5744261a056881b14cb36848
Davide Buoso <daviibuoso@gmail.com>
2024-02-13 17:08:12 +0100

    original dataset

--- legal_ner/utils/dataset.py
+++ legal_ner/utils/dataset.py
@@ -13,25 +13,22 @@ nlp = spacy.load("en_core_web_sm")
 #                      DATASET CLASS                       #
 #                                                          #
 ############################################################ 
-
 class LegalNERTokenDataset(Dataset):
+    
     def __init__(self, dataset_path, model_path, labels_list=None, split="train", use_roberta=False):
-        self.model_path = model_path
-        self._data = json.load(open(dataset_path))
-        self._split = split
+        self.data = json.load(open(dataset_path))
+        self.split = split
         self.use_roberta = use_roberta
-        print("Using roberta config" if use_roberta else "Not using Roberta config")
-        self.column_names = ["tokens", "ner_tags"]
         if self.use_roberta:     ## Load the right tokenizer
             self.tokenizer = RobertaTokenizerFast.from_pretrained("roberta-base")
         else:
             self.tokenizer = AutoTokenizer.from_pretrained(model_path) 
         self.labels_list = sorted(labels_list + ["O"])[::-1]
+
         if self.labels_list is not None:
             self.labels_to_idx = dict(
                 zip(sorted(self.labels_list)[::-1], range(len(self.labels_list)))
             )
-        print(self.labels_list)
 
     def __len__(self):
         return len(self.data)
@@ -39,6 +36,7 @@ class LegalNERTokenDataset(Dataset):
     def __getitem__(self, idx):
         item = self.data[idx]
         text = item["data"]["text"]
+
         ## Get the annotations
         annotations = [
             {
@@ -50,35 +48,41 @@ class LegalNERTokenDataset(Dataset):
         ]
 
         ## Tokenize the text
-        inputs = self.tokenizer(
-            text, 
-            return_tensors="pt", 
-            truncation=True, 
-            verbose=False,
-            padding='max_length'
-        )
+        if not self.use_roberta:
+            inputs = self.tokenizer(
+                text, 
+                return_tensors="pt", 
+                truncation=True, 
+                verbose=False
+                )
+        else:
+            inputs = self.tokenizer(
+                text, 
+                return_tensors="pt", 
+                truncation=True, 
+                verbose=False, 
+                padding='max_length'
+            )
 
         ## Match the labels
         aligned_labels = match_labels(inputs, annotations)
         aligned_labels = [self.labels_to_idx[l] for l in aligned_labels]
         inputs["input_ids"] = inputs["input_ids"].squeeze(0).long()
         inputs["attention_mask"] = inputs["attention_mask"].squeeze(0).long()
-        #print(self.use_roberta)
-        if inputs.get("token_type_ids") != None:
+        if not self.use_roberta:
             inputs["token_type_ids"] = inputs["token_type_ids"].squeeze(0).long()
 
-        if "span" in self.model_path:
-            inputs["tokens"] = self.tokenizer.decode(inputs["input_ids"])
         ## Get the labels
         if self.labels_list:
             labels = torch.tensor(aligned_labels).squeeze(-1).long()
-            column_name = "labels" if "span" not in self.model_path else "ner_tags"
+
             if labels.shape[0] < inputs["attention_mask"].shape[0]:
                 pad_x = torch.zeros((inputs["input_ids"].shape[0],))
                 pad_x[: labels.size(0)] = labels
-                inputs[column_name] = aligned_labels
+                inputs["labels"] = aligned_labels
             else:
-                inputs[column_name] = labels[: inputs["attention_mask"].shape[0]]
+                inputs["labels"] = labels[: inputs["attention_mask"].shape[0]]
+
         return inputs
 
 ## FOR SPAN
