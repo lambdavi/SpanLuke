ac69ed01ccfbef50fa8cfbaa230014946838f5e7
Davide Buoso <daviibuoso@gmail.com>
2024-02-17 17:41:02 +0100

    updated tokenizer

--- legal_ner/utils/ener.py
+++ legal_ner/utils/ener.py
@@ -75,7 +75,7 @@ class ENER_DataProcessor():
 
 
     def tokenize_and_align_labels(self, examples, label_all_tokens=True): 
-        tokenized_inputs = self.tokenizer(examples["tokens"], truncation=True, is_split_into_words=True) 
+        tokenized_inputs = self.tokenizer(examples["tokens"], truncation=True, is_split_into_words=True, return_tensors="pt", padding='max_length') 
         labels = [] 
         for i, label in enumerate(examples["ner_tags"]): 
             word_ids = tokenized_inputs.word_ids(batch_index=i) 
