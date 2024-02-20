4e9f56616b6d56efb41fad4ce5ed9279310a3e1b
Davide Buoso <daviibuoso@gmail.com>
2024-02-20 15:01:20 +0100

    test new ener tok

--- legal_ner/main_ener.py
+++ legal_ner/main_ener.py
@@ -549,7 +549,7 @@ if __name__ == "__main__":
         )
 
         ## Collator
-        data_collator = DefaultDataCollator() if dataset != "ener" else DataCollatorWithPadding(model.tokenizer)
+        data_collator = DefaultDataCollator() if dataset != "ener" else DataCollatorWithPadding(train_ds.tokenizer)
 
         ## Trainer
         trainer = Trainer(
