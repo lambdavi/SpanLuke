b5a02f40e39905837511ec14d95a034f4297884f
Davide Buoso <daviibuoso@gmail.com>
2024-02-21 08:08:43 +0100

    new_test

--- legal_ner/main_ener.py
+++ legal_ner/main_ener.py
@@ -591,7 +591,8 @@ if __name__ == "__main__":
         )
 
         ## Collator
-        data_collator = DefaultDataCollator() if dataset != "ener" else DataCollatorForTokenClassification(tokenizer=data_processor.tokenizer, padding=False)
+        data_collator = DefaultDataCollator() 
+        # if dataset != "ener" else DataCollatorForTokenClassification(tokenizer=data_processor.tokenizer, padding=False)
 
 
         ## Trainer
