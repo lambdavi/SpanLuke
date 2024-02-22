91d0f2042823b782d4f04b37ec5e6a93a82ad1d9
Davide Buoso <daviibuoso@gmail.com>
2024-02-22 18:12:05 +0100

    test tokenizer

--- legal_ner/main_ener.py
+++ legal_ner/main_ener.py
@@ -501,11 +501,11 @@ if __name__ == "__main__":
 
         model.set_tokenizer(tokenizer)
         print(model.tokenizer.tokenizer)
-        
+
         if dataset =="legal_ner":
             span_dataset = load_legal_ner(ds_train_path, ds_valid_path)
         else:
-            data_processor = ENER_Dataset(ds_train_path, ds_valid_path, tokenizer = tokenizer, labels_list=labels_list)
+            data_processor = ENER_Dataset(ds_train_path, ds_valid_path, tokenizer = model.tokenizer, labels_list=labels_list)
             span_dataset = data_processor.get_ener_dataset()
             
 
